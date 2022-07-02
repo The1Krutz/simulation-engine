fdc6bd6a77a764cfc7aaf4b1d473e1b4ec4aa89c
Thomas Kent <the1krutz@gmail.com>
2022-07-01 22:47:46 -0700

    forgot most of my C# =/

--- Simulation/Class1.cs
+++ Simulation/Class1.cs
@@ -1,6 +1,26 @@
 ﻿namespace Simulation;
 
-public class Class1 {
+public class Engine<T> {
+  private int CurrentTick = 0;
+  private List<T> SimulationHistory;
 
+  public Engine(T defaultState) {
+    SimulationHistory = new List<T> { defaultState };
+  }
+
+  public T GetStateAtTick(int tick) {
+    if (tick < SimulationHistory.Count) {
+      return SimulationHistory[tick];
+    } else {
+      return GenerateNextTick();
+    }
+  }
+
+  public T GenerateNextTick() {
+    throw new NotImplementedException();
+  }
 }
 
+public class SimulationState {
+
+}
